996c9a90c3b996a02783124626ed4f58a81f2dd8
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2023-11-30 16:41:23 -0500

    Se agregaron comentarios utiles para entender el codigo de mejor manera

--- Multidimencional.c
+++ Multidimencional.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
+//Libreria que se uso
 
 int main(){
 int n,m,o,p;
+//Variables que se van a usar
 printf("Coloque el numero de matrices \n");
 scanf("%d", &o);
+//numero de matrices
 printf("Coloque los datos de n\n");
 scanf("%d", &n);
+//tamaño de las matrices
 printf("Coloque los datos de m\n");
 scanf("%d", &m);
+//tamaño de las matrices
 int matrix[n][m];
 o=o-1;
+//se le resta uno al numero de matrices que pide el usuario
 for (int k = 0; k < o; k++)
+//imprime el numero de matrices -1 de las que pone el usuario
 {   
     p=k;
+    //se le da el valor de ka a otra variable
     printf("\n");
     printf("\n");
     printf("Matriz numero %d\n",k);
+    //printf para poder imprimir el numero de matrices 
 
 
     for (int i = 0; i < n; i++)
@@ -30,6 +39,7 @@ for (int k = 0; k < o; k++)
     
 }
 }
+//se imprime la matriz que falta llena de unos 
     p=p+1;
     printf("\n");
     printf("\n");
