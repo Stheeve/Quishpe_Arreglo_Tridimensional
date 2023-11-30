2a93e0485ae8511ecce7ca00f96d1848234a8e73
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2023-11-30 16:31:55 -0500

    Se inicializo las variables a usar

---
+++ Multidimencional.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(){
+srand (time(NULL));
+int n,m,o,p;
+printf("Coloque el numero de matrices \n");
+scanf("%d", &o);
+printf("Coloque los datos de n\n");
+scanf("%d", &n);
+printf("Coloque los datos de m\n");
+scanf("%d", &m);
+int matrix[n][m];
+
+}
+
+    
+
+
